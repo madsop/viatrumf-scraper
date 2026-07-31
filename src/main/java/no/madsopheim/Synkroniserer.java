ef9d3e2dcf7b93d511ca73e9da9e896c7165b9bc
Mads Opheim <mads.opheim@gmail.com>
2026-07-31 23:04:54 +0200

    Generaliserer eitt hakk til

--- src/main/java/no/madsopheim/Synkroniserer.java
+++ src/main/java/no/madsopheim/Synkroniserer.java
@@ -16,7 +16,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
-import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Dependent
@@ -42,26 +42,23 @@ class Synkroniserer {
     }
 
     private void synkroniserTrumfNetthandel() throws IOException, ExecutionException, InterruptedException {
-        synkroniser("a.merchant-tile", "https://trumfnetthandel.no/category/paged/all/500", "viatrumf-scraper2", this::formaterTrumfNetthandelInnslag);
+        synkroniser("a.merchant-tile", "https://trumfnetthandel.no/category/paged/all/500", "viatrumf-scraper2", TrumfNetthandelInnslag::create);
     }
 
-    private <T extends Innslag> void synkroniser(String selectFilter, String url, String collectionNamn, BiFunction<Element, ZonedDateTime, T> biFunction) throws IOException, ExecutionException, InterruptedException {
+    private <T extends Innslag> void synkroniser(String selectFilter, String url, String collectionNamn, Function<SynkroniseringInternalRequest, T> function) throws IOException, ExecutionException, InterruptedException {
         var no = ZonedDateTime.now(ZoneId.of(tidssone));
         var futures = new ArrayList<ApiFuture<?>>();
         for (Element butikk : Jsoup.connect(url).get().select(selectFilter)) {
-            Innslag innslag = biFunction.apply(butikk, no);
+            Innslag innslag = function.apply(new SynkroniseringInternalRequest(butikk, no, formatter));
             futures.add(lagring.lagre(innslag, collectionNamn));
         }
         IO.println("Håndterer " + futures.size() + " innslag");
         ApiFutures.allAsList(futures.stream().filter(Objects::nonNull).collect(Collectors.toList())).get();
     }
-
-    private Innslag formaterTrumfNetthandelInnslag(Element butikk, ZonedDateTime no) {
-        String namn = butikk.attribute("data-name").getValue();
-        String verdi = butikk.attribute("data-percentage").getValue();
-        String popularitet = butikk.attribute("data-popularity").getValue();
-        String href = butikk.attribute("href").getValue();
-        String timestamp = no.format(formatter);
-        return new TrumfNetthandelInnslag(namn, verdi, href, popularitet, timestamp);
-    }
 }
+
+record SynkroniseringInternalRequest(
+    Element element,
+    ZonedDateTime no,
+    DateTimeFormatter formatter
+) {}
