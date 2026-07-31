84f13c54eb24da28f99e90392acb4e07b621a69c
Mads Opheim <mads.opheim@gmail.com>
2026-07-31 22:47:09 +0200

    Startar generalisering

--- src/main/java/no/madsopheim/Synkroniserer.java
+++ src/main/java/no/madsopheim/Synkroniserer.java
@@ -24,9 +24,7 @@ class Synkroniserer {
     @Inject
     Lagring lagring;
 
-    private static final String selectFilter = "a.merchant-tile";
     private static final String tidssone = "Europe/Oslo";
-    private static final String url = "https://trumfnetthandel.no/category/paged/all/500";
 
     @PostConstruct
     void init() {
@@ -37,17 +35,27 @@ class Synkroniserer {
     private DateTimeFormatter formatter;
 
     void synkroniser() throws IOException, ExecutionException, InterruptedException {
+        IO.println("Starter synkronisering av Trumf Netthandel");
+        synkroniserTrumfNetthandel();
+        IO.println("Ferdig med å synkronisere Trumf Netthandel");
+    }
+
+    private void synkroniserTrumfNetthandel() throws IOException, ExecutionException, InterruptedException {
+        synkroniser("a.merchant-tile", "https://trumfnetthandel.no/category/paged/all/500", "viatrumf-scraper2");
+    }
+
+    private void synkroniser(String selectFilter, String url, String collectionNamn) throws IOException, ExecutionException, InterruptedException {
         var no = ZonedDateTime.now(ZoneId.of(tidssone));
         var futures = new ArrayList<ApiFuture<?>>();
         for (Element butikk : Jsoup.connect(url).get().select(selectFilter)) {
-            Innslag innslag = formaterInnslag(butikk, no);
-            futures.add(lagring.lagre(innslag, "viatrumf-scraper2"));
+            Innslag innslag = formaterTrumfNetthandelInnslag(butikk, no);
+            futures.add(lagring.lagre(innslag, collectionNamn));
         }
         IO.println("Håndterer " + futures.size() + " innslag");
         ApiFutures.allAsList(futures.stream().filter(Objects::nonNull).collect(Collectors.toList())).get();
     }
 
-    private Innslag formaterInnslag(Element butikk, ZonedDateTime no) {
+    private Innslag formaterTrumfNetthandelInnslag(Element butikk, ZonedDateTime no) {
         String namn = butikk.attribute("data-name").getValue();
         String verdi = butikk.attribute("data-percentage").getValue();
         String popularitet = butikk.attribute("data-popularity").getValue();
